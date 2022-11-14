cbbf86a1217966388f6d8cb184060c1a1707527c
Theologos Mavropoulos <rusinner88@gmail.com>
2022-11-14 22:20:12 +0200

    buttons-ready

--- tabs-project/src/App.js
+++ tabs-project/src/App.js
@@ -25,8 +25,44 @@ function App() {
       </section>
     );
   }
-
-  return <div className="App">jobs</div>;
+  const { company, dates, duties, title } = jobs[value];
+  return (
+    <section className="App">
+      <div className="title">
+        <h2>experience</h2>
+        <div className="underline"></div>
+      </div>
+      <div className="jobs-center">
+        <div className="btn-container">
+          {jobs.map((item, index) => {
+            return (
+              <button
+                key={item.id}
+                onClick={() => {
+                  setValue(index);
+                }}
+              >
+                {item.company}
+              </button>
+            );
+          })}
+        </div>
+        <article className="job-info">
+          <h3>{title}</h3>
+          <h4>{company}</h4>
+          <p className="job-date">{dates}</p>
+          {duties.map((duty, index) => {
+            return (
+              <div key={index} className="job-desc">
+                <FaAngleDoubleRight className="job-icon" />
+                <p>{duty}</p>
+              </div>
+            );
+          })}
+        </article>
+      </div>
+    </section>
+  );
 }
 
 export default App;
